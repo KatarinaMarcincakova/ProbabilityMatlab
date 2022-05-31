9b45eaadadc339e59bacbffd18b4885563dafabb
Katka Marcincakova <katulik137@gmail.com>
2022-05-31 15:37:37 +0200

    run exe from .m file

--- Zadanie3.m
+++ Zadanie3.m
@@ -1,1 +1,6 @@
-system('dist/a/a.exe');
+clc;
+disp('Spusta sa aplikacia.');
+disp('Ak sa nahodou nezobrazi cast okna v ktorej sa da minimalizovat alebo zavriet staci resizovat okno.');
+disp('Z nejakeho dovodu MATLAB obcas zobrazi okno tak ze nevidno navigacny panel.');
+disp('NMPaS - Zadanie 3 - 1.0.0 - 2022');
+!start Zadanie3App\for_redistribution_files_only\Zadanie3App.exe
