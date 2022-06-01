67148cc449b8300b87d6b84dabe6910cb27867f5
Katka Marcincakova <katulik137@gmail.com>
2022-06-01 06:47:12 +0200

    zaloha

--- Zadanie3.m
+++ Zadanie3.m
@@ -1,6 +1,6 @@
 clc;
 disp('Spusta sa aplikacia.');
-disp('Ak sa nahodou nezobrazi cast okna v ktorej sa da minimalizovat alebo zavriet staci resizovat okno.');
-disp('Z nejakeho dovodu MATLAB obcas zobrazi okno tak ze nevidno navigacny panel.');
+disp('Ak sa nahodou nezobrazi cast okna v ktorej sa da preklikavat sa medzi jednotlivymi oknami uloh a minimalizovat alebo zavriet staci resizovat okno na fullscreen (cez sipku ktora sa ukaze ked sa prejde ponad okraj okna).');
+disp('Z nejakeho dovodu MATLAB obcas zobrazi okno tak ze nevidno navigacny panel (moznosti preklikavat sa medzi jednotlivymi oknami uloh)');
 disp('NMPaS - Zadanie 3 - 1.0.0 - 2022');
 !start Zadanie3App\for_redistribution_files_only\Zadanie3App.exe
